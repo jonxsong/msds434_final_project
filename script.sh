e4f164087387be6c36432ae799deaadde461d888
JZ <you@example.com>
2022-11-26 20:56:44 +0000

    debugging...

--- script.sh
+++ script.sh
@@ -28,18 +28,18 @@ export ENDPOINT_ID=census_bureau_acs_endpoint
 
 echo "${PROJECT_ID} ${ENDPOINT_ID}"
 
-curl \
--X POST \
--H "Authorization: Bearer $(gcloud auth print-access-token)" \
--H "Content-Type: application/json" \
-https://us-west1-aiplatform.googleapis.com/v1/projects/${PROJECT_ID}/locations/us-west1/endpoints/${ENDPOINT_ID}:predict \
--d "@${INPUT_DATA_FILE}"
+#curl \
+#-X POST \
+#-H "Authorization: Bearer $(gcloud auth print-access-token)" \
+#-H "Content-Type: application/json" \
+#https://us-west1-aiplatform.googleapis.com/v1/projects/${PROJECT_ID}/locations/us-west1/endpoints/${ENDPOINT_ID}:predict \
+#-d "@${INPUT_DATA_FILE}"
 
 curl \
 -X GET \
 -H "Authorization: Bearer $(gcloud auth print-access-token)" \
 -H "Content-Type: application/json" \
-https://us-west1-aiplatform.googleapis.com/v1/projects/${PROJECT_ID}/locations/us-west1/endpoints/${ENDPOINT_ID} \
+https://us-west1-aiplatform.googleapis.com/v1/projects/${PROJECT_ID}/locations/us-west1/endpoints/${ENDPOINT_ID}:explain\
 -d "@${INPUT_DATA_FILE}"
 
 #echo https://us-west1-aiplatform.googleapis.com/v1/projects/msds434finalproject/locations/us-west1/endpoints/census_bureau_acs_endpoint:predict
